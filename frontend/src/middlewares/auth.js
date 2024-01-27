42aabeecd52c29b49c1f880190275d4d82aad954
Fran Caballero <francisco_envire@hotmail.com>
2024-01-27 17:42:45 +0100

    fix: unauthorized screen fixes

--- frontend/src/middlewares/auth.js
+++ frontend/src/middlewares/auth.js
@@ -1,17 +1,24 @@
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
+import { Outlet, NavLink } from 'react-router-dom'
 import { useEffect } from 'react'
 
 const Auth = () => {
   const { userInfo } = useSelector((state) => state.auth)
   const navigate = useNavigate()
 
+  // show unauthorized screen if no user is found in redux store
   if (!userInfo) {
-    navigate('/login')
-  } else {
-    return <Outlet />
+    return (
+      <div className='unauthorized'>
+        <h1>Unauthorized :(</h1>
+        <span>
+          <NavLink to='/login'>Login</NavLink> to gain access
+        </span>
+      </div>
+    )
   }
+  return <Outlet />
 
 }
 
