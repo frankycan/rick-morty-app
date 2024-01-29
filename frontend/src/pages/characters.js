bfa9a12f11e28a94ac2f29f24dad371ef02580a9
Fran Caballero <francisco_envire@hotmail.com>
2024-01-29 20:30:39 +0100

    feat: maintain pagination on refresh

--- frontend/src/pages/characters.js
+++ frontend/src/pages/characters.js
@@ -2,26 +2,32 @@ import React, { useEffect, useState } from "react"
 import { getCharacters } from "../services/charactersService.js"
 import CharactersList from "../components/charactersList.js"
 import Pagination from "../components/pagination.js"
+import { useSearchParams  } from 'react-router-dom'
 import '../styles/characters.css'
 
 const Characters = () => {
   const [characters, setCharacters] = useState([])
-  const [page, setPage] = useState(1)
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlPage = Number(searchParams.get('page')) || 1;
+  const [page, setPage] = useState(urlPage)
   const [lastPage, setLastPage] = useState(0)
 
   const getCharactersByPage = async () => {
-    const { results, info } = await getCharacters(page)
+    const { results, info } = await getCharacters(urlPage)
     setCharacters(results)
     setLastPage(info.pages)
   };
   useEffect(() => {
     getCharactersByPage()
+    setSearchParams({ page: urlPage })
   }, [page]);
 
   const increasePage = () => {
+    setSearchParams({ page: page + 1 })
     setPage(page + 1);
   };
   const decreasePage = () => {
+    setSearchParams({ page: page - 1 })
     setPage(page - 1);
   };
   return (
